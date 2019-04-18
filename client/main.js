4c1f47d0bff024c07df91e9257adf0eb4a6c20a5
Houssem Trabelsi <trabelsi.houssem93@gmail.com>
2019-04-18 17:14:09 +0100

    Update main.js

--- client/main.js
+++ client/main.js
@@ -6,6 +6,8 @@ function led(etat) {
     if (etat == 'undefined') {
         return;
     }
+    //here is to create the object that can send data to the raspberry pi
+    //from your raspberry
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", "http://192.168.1.4:8089", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
